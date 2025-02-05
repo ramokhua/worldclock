f118c0368fd34dce92a8cf5c636f9c8b95b3f5b6
Boitsholo Ramokhua <ramokhuaboitsholo@gmail.com>
2025-02-05 14:39:38 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -45,11 +45,9 @@ function updateCity(event) {
   <div class="city">
     <div>
       <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
   </div>
   <a href="/">All cities</a>
   `;
