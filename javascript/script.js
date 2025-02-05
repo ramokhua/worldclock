e13896046557e97563798eca7b5ed185986d373b
Boitsholo Ramokhua <ramokhuaboitsholo@gmail.com>
2025-02-05 14:18:13 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -33,6 +33,7 @@ function updateTime() {
   }
 }
 
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -41,22 +42,17 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  
-  // Hide all cities
-  let allCities = document.querySelectorAll(".city");
-  allCities.forEach(city => {
-    city.style.display = 'none';
-  });
-  
-  // Display the selected city
   citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
   </div>
+  <a href="/">All cities</a>
   `;
 }
 
