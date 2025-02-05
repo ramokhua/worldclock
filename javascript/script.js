02fe994eff63d97aeb657057bc122584b999a235
Boitsholo Ramokhua <ramokhuaboitsholo@gmail.com>
2025-02-05 14:07:20 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -58,5 +58,5 @@ function updateCity(event) {
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("#city-select");
+let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
